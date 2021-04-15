e85685d24afecd5ddc2444e4c4e5925ae10ab4c3
dunglak2001 <82558683+dunglak2001@users.noreply.github.com>
2021-04-16 00:17:16 +0700

    Add files via upload

---
+++ ptb2.java
@@ -0,0 +1,10 @@
+package GiaiPhuongTrinhBac2;
+
+public class ptb2 {
+	
+	    public static void main(String[] args) {
+	        ptb2view view = new ptb2view();
+	       ptb2controller controller = new ptb2controller(view);
+	    }
+	}
+
